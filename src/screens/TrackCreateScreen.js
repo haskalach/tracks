0e7363dd38c7ba2473134b036453b6dc4a1a712d
hassan kalach <hassan_kalach@hotmail.com>
2020-07-16 19:24:22 +0300

    adding all screens and conneting them with navigation properties

---
+++ src/screens/TrackCreateScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+
+const TrckCreateScreen = () => {
+    return <Text style={{ fontSize: 48 }}>TrckCreateScreen</Text>
+}
+
+const styles = StyleSheet.create({});
+
+export default TrckCreateScreen;
